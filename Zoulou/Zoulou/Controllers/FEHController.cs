cbda041bab65dfac3900b622c8120eac2d3047c7
chancho60 <1169732@gmail.com>
2018-01-03 18:23:29 -0500

    merged

--- Zoulou/Zoulou/Controllers/FEHController.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Web.Mvc;
-
-namespace Zoulou.Controllers {
-    public class FEHController : BaseController {
-        public ActionResult Index() {
-            return View();
-        }
-    }
-}
