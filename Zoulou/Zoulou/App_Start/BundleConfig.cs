2c689d754f4cf1fec0c7f07e7c262c82d87535f0
Vincent Tremblay <TheBearGame@Gmail.com>
2018-01-09 17:31:38 -0500

    MDL try out

--- Zoulou/Zoulou/App_Start/BundleConfig.cs
+++ Zoulou/Zoulou/App_Start/BundleConfig.cs
@@ -6,28 +6,23 @@ namespace Zoulou
     public class BundleConfig
     {
         // Pour plus d'informations sur le regroupement, visitez https://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
-        {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+        public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new ScriptBundle("~/Bundles/scripts").Include(
+                "~/Content/Scripts/jquery-{version}.js",
+                "~/Content/Scripts/jquery.validate*",
+                "~/Content/Scripts/material.min.js",
+                "~/Content/Scripts/respond.js"));
 
             // Utilisez la version de développement de Modernizr pour le développement et l'apprentissage. Puis, une fois
             // prêt pour la production, utilisez l'outil de génération à l'adresse https://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/Bundles/modernizr").Include(
+                "~/Content/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/PKM.css",
-                      "~/Content/rpg-awesome.css"));
+                "~/Content/Css/material.min.css",
+                "~/Content/Css/site.css",
+                "~/Content/Css/PKM.css",
+                "~/Content/Css/rpg-awesome.css"));
         }
     }
 }
